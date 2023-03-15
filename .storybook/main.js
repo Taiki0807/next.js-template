f60fa8b59cd866b0d08d39f7259ab6839386a51a
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-15 13:10:20 +0900

    feat(storybook): upgrade to version 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,31 +1,27 @@
 var path = require('path');
 module.exports = {
-  stories: [
-    '../app/**/*.stories.mdx',
-    '../app/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  stories: ['../app/**/*.stories.mdx', '../app/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   features: {
-    interactionsDebugger: true,
+    interactionsDebugger: true
   },
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
   },
-  webpackFinal: (config) => {
+  webpackFinal: config => {
     return {
       ...config,
       resolve: {
         ...config.resolve,
         alias: {
           ...config.resolve.alias,
-          '@': path.resolve(__dirname, '../'),
-        },
-      },
+          '@': path.resolve(__dirname, '../')
+        }
+      }
     };
   },
+  docs: {
+    autodocs: true
+  }
 };
