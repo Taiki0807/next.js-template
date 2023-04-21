74015baf364d7f5d5b25d439b84bd5741e8d31aa
Taiki0807 <hosokawataiki265@gmail.com>
2023-04-21 16:23:03 +0900

    fix: add newline at the end of the file

--- app/components/parts/Button/Button.test.tsx
+++ app/components/parts/Button/Button.test.tsx
@@ -3,10 +3,10 @@ import { render, screen } from '@testing-library/react';
 import * as stories from './Button.stories';
 import '@testing-library/jest-dom';
 
-const { Default } = composeStories(stories)
+const { Default } = composeStories(stories);
 
 test('render Button with default args', () => {
   render(<Default>Button</Default>);
   const ButtonElement = screen.getByText(/Button/i);
   expect(ButtonElement).toBeInTheDocument();
-})
+});
